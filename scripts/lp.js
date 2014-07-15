b8ae40ed9740c54fef86aceb3959cd350b79b480
dcoloma-usj <dcoloma@tid.es>
2014-07-15 10:03:32 +0200

    First pages commit

---
+++ scripts/lp.js
@@ -0,0 +1,8 @@
+function pLPCommand(c, a1, a2, a3, a4) {
+parent.window["pLPCommand6"] && parent.window["pLPCommand6"](c, a1, a2, a3, a4);
+}
+function pRTLPCB(pN, data) {
+parent.window["pRTLPCB6"] && parent.window["pRTLPCB6"](pN, data);
+}
+pRTLPCB(0,[{"d":{"t":"r","d":"s-dal5-nss-16.firebaseio.com"},"t":"c"}]);
+pLPCommand('close');
